d4e4fd9bc06da1cfbdc5476e31fb89df698dc72f
Siddharth Subramaniam <siddharthsubramaniam1996@gmail.com>
2016-09-17 19:07:57 +0530

    Add files via upload

---
+++ WEIRD.C
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+
+{
+
+int i,n,arr[50],x=1;
+clrscr();
+
+
+
+printf("enter the limit to which the numbers should be printed \nfrom 1 onwards : ");
+scanf("%d",&n);
+
+for(i=0;i<n;i++)
+
+{
+
+	printf("%d\n",x);
+	x=x+2;
+}
+getch();
+
+}
+
